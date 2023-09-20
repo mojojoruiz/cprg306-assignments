a3ae1d9f64756c1f607569c2985e6a669f59dfb6
Johannah Ruiz <131231470+mojojoruiz@users.noreply.github.com>
2023-09-20 09:36:33 -0600

    StudentInfo.js

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -8,7 +8,7 @@ return (
     <h2>Johannah Ruiz</h2>
     <p>CPRG 306 B</p>
     <Link href="https://github.com/mojojoruiz">
-        <a>Link to GitHub repository</a>
+        Link to GitHub repository
     </Link>
     </div>
 );
